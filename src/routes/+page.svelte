185ad9c8a0fa32cea40b0b69336174fff2671de8
Jcrecksi <117695656+Jcrecksi@users.noreply.github.com>
2024-03-09 21:22:00 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
     <p class="text-center pb-9 text-lg font-bol text-black lg:text-xl dark:text-balck">Introducing WeekendPlanner: Your personalized itinerary creator for unforgettable weekends! Discover, accept, and compile exciting activities tailored to your preferences.</p>
     
     <a href="../plans" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-        Learn more
+        Generate Plan
         <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
       </svg>
